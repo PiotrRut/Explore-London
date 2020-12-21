632df866470bdf8dfe7f689e14136fdbb6c217e0
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-12-21 01:02:26 +0100

    Initial Commit

---
+++ Explore/ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreApp.swift
+//  Explore
+//
+//  Created by Piotr Rutkowski on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
